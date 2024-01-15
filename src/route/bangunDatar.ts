aa9442280ea359affd0c9c8043eb19f9f2a0defa
Cinta Holland <cintaholland@21gmail.com>
2024-01-15 13:20:20 +0700

    ini adalah commit yang pertama

---
+++ src/route/bangunDatar.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import { kelilingLingkaran, kelilingPersegi, kelilingPersegiPanjang, luasLingkaran, luasPersegi, luasPersegiPanjang, luasSegitiga } from "../controller/bangunDatar"
+const app = express()
+
+/**  allow read a body */
+app.use(express.json())
+
+/** fungsi use digunakan untuk menerapkan sebuah fungsi pada object express. 
+ * fungsi tersebut akan otomatis dijalankan */
+
+app.post(`/lingkaran/luas`, luasLingkaran)
+app.post(`/lingakaran/keliling`, kelilingLingkaran)
+app.post(`/Persegi/luas`,luasPersegi)
+app.post(`/Persegi/keliling`,kelilingPersegi)
+app.post(`PersegiPanjang/luas`,luasPersegiPanjang)
+app.post(`PersegiPanjang/keliling`,kelilingPersegiPanjang)
+app.post(`/Segitiga/luas`,luasSegitiga)
+export default app 
---
+++ src/route/bangunRuang.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { volumeTabung, luaspermukaanTabung, volumeKubus, luaspermukaanKubus , volumeBalok, luaspermukaanBalok, volumeBola, luaspermukaanBola } from "../controller/bangunRuang"
+const app = express()
+
+app.use(express.json())
+
+app.post(`/Tabung/volume`, volumeTabung)
+app.post(`/Tabung/luaspermukaan`, luaspermukaanTabung)
+app.post(`/Kubus/volume`,volumeKubus)
+app.post(`/Kubus/luaspermukaan`,luaspermukaanKubus)
+app.post(`Balok/volume`,volumeBalok)
+app.post(`Balok/luaspermukaan`,luaspermukaanBalok)
+app.post(`/Bola/volume`,volumeBola)
+app.post(`/Bola/luaspermukaan`,luaspermukaanBola)
+export default app 
